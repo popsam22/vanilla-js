e69272682f65536a84094628bf214ef4fef16127
popsam22 <97094431+popsam22@users.noreply.github.com>
2022-10-10 04:51:55 -0700

    Add files via upload

---
+++ templateliteral.js
@@ -0,0 +1,21 @@
+
+var num1 = 20;
+var num2 = 12;
+
+var result = num1 + num2 ;
+
+//using concatenation
+
+console.log("This addition of " + num1 + " and " + num2 + " is " + result);
+
+//using template literal by using backtick
+
+console.log(`This addition of ${num1} and ${num2} is ${result}`);
+
+//another example
+
+console.log(`I love :
+-spaghetti
+-turkey
+-beans`)
+
---
+++ ternary.js
@@ -0,0 +1,10 @@
+//A shoter version of finding if a num is even or odd using ternary op
+
+
+let num = 17;
+let result;
+
+// ternary operator = ?: best used in a two fork way 
+
+result = num%2===0 ? "Even" : "Odd";
+console.log(result);
